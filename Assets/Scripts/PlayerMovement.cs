772374a347e385f3be71fb3e9c560ec13b9bf240
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-10-01 13:31:45 -0300

    Jump animations added and working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ public class Movement : MonoBehaviour
     [SerializeField] private float speed;
     
     private Rigidbody2D body;
-    private Animator anim; 
-
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
@@ -33,15 +33,26 @@ public class Movement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        
-        
-        if(Input.GetKey(KeyCode.UpArrow))
-        {
-            body.velocity = new Vector2(body.velocity.x,speed);
-        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow) && grounded)
+            Jump();
 
         anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
 
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        grounded = false;
+        anim.SetTrigger("jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 
 }
