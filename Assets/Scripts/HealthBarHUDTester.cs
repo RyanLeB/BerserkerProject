b84e5ffa29d2dfe291eee1f8a683cc0952c5fd60
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-11-01 14:32:19 -0300

    health system started

---
+++ Assets/Scripts/HealthBarHUDTester.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class HealthBarHUDTester : MonoBehaviour
+{
+    public void AddHealth()
+    {
+        PlayerStats.Instance.AddHealth();
+    }
+
+    public void Heal(float health)
+    {
+        PlayerStats.Instance.Heal(health);
+    }
+
+    public void Hurt(float dmg)
+    {
+        PlayerStats.Instance.TakeDamage(dmg);
+    }
+}
