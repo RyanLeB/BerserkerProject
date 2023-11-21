8c874a4c05416e351718124597b6622358a163e3
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-11-21 16:31:39 -0400

    Health system and text started

--- Assets/Scripts/DamageTaken.cs
+++ Assets/Scripts/DamageTaken.cs
@@ -1,15 +1,51 @@
 
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class DamageTaken : MonoBehaviour
 {
+    [SerializeField] AudioSource hurtSFX;
+    
+    static public int health = 3;
+    public Text healthText;
+
+    private void Update()
+    {
+        healthText.text = health.ToString();
+
+
+    }
+
+
+
+
+    static void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+
+
     [SerializeField] private string newLevel;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("VillageLVL");
+            TakeDamage(1);
+            hurtSFX.Play();
+
+            if(health == 0)
+            {
+                SceneManager.LoadScene("VillageLVL");
+                health = 3;
+            }
         }
+        
+        
+        
+            
+          
+        
     }
 }
