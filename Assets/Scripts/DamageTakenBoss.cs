b2f97d3f5686309551fb1974182c57310b4837fd
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-12-04 21:24:55 -0400

    boss fight theme added

--- Assets/Scripts/DamageTakenBoss.cs
+++ Assets/Scripts/DamageTakenBoss.cs
@@ -7,9 +7,13 @@ public class DamageTakenBoss : MonoBehaviour
 {
     [SerializeField] AudioSource hurtSFX;
 
-    static public int health = 100;
+    static public int health = 20;
     public Text healthText;
+    public float timestamp = 0f;
+    public float delay = 1f;
+    
 
+    
     private void Update()
     {
         if (healthText != null)
@@ -26,25 +30,30 @@ public class DamageTakenBoss : MonoBehaviour
 
     private void TakeDamage(int damage)
     {
-        health -= damage;
+        if (Time.time > timestamp + delay)
+        {
+            hurtSFX.Play();
+            health -= damage;
+            timestamp = Time.time;
+        }
         
     }
 
 
 
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            TakeDamage(5);
-            hurtSFX.Play();
+            TakeDamage(1);
+            
 
             if (health == 0)
             {
 
                 SceneManager.LoadScene("BossLVL");
-                health = 100;
+                health = 20;
             }
         }
 
