c6892fd1876c6fdecf9723276d8e6af125c1bb63
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-12-05 19:40:25 -0400

    fixed bug with spikes not working

---
+++ Assets/Scripts/DamageTakenFantasySpike.cs
@@ -0,0 +1,55 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class DamageTakenFantasySpike : MonoBehaviour
+{
+    [SerializeField] AudioSource hurtSFX;
+
+    static public int health = 3;
+    public Text healthText;
+
+    private void Update()
+    {
+        if (healthText != null)
+        {
+            healthText.text = health.ToString();
+
+        }
+
+
+    }
+
+
+
+
+    static void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TakeDamage(3);
+            hurtSFX.Play();
+
+            if (health == 0)
+            {
+
+                SceneManager.LoadScene("FantasyLVL");
+                health = 3;
+            }
+        }
+
+
+
+
+
+
+    }
+}
